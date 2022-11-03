e91c665f906bd4f9bb89c62191ee7e5c011786b6
김하늘 <80873494+hanul1206@users.noreply.github.com>
2022-11-03 11:12:00 +0900

    auto 적용

--- G2_2/08-02 map.cpp
+++ G2_2/08-02 map.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include <string>
-#include <vector>
 #include <map>
 
 using namespace std;
 
-// 김신 : 공유
-// 지은탁 : 김고은
-// 저승사자 : 이동욱
-// 써니 : 유인나
+//김신 : 공유
+//지은탁 : 김고은
+//저승이 : 이동욱
+//써니 : 유인나
 
 // arr["김신"] = "공유"
 // arr["지은탁"] = "김고은"
 
 int main(void)
 {
-	// map<key자료형, value 자료형>
-	map<string, string> m;
-	m["김신"] = "공유";
-	m["지은탁"] = "김고은";
-	m["저승사자"] = "이동욱";
-	m["써니"] = "유인나";
+    // map<key 자료형, value자료형>
+    map<string, string> m;
+    m["김신"] = "공유";
+    m.insert(pair<string, string>("지은탁", "김고은"));
+    m.insert(pair<string, string>("저승사자", "이동욱"));
+    m.insert(pair<string, string>("써니", "유인나"));
 
-	map<string, string>::iterator iter;
-	
-	// 백터의 원소들을 출력(iterator)
-	// 삽입할 떄마다 key는 자동정렬
-	for (iter = m.begin(); iter != m.end(); iter++) {
-		cout << iter->first << "역 " << iter->second << "님 " << endl;
-	}
-	cout << endl;
+    map<string, string>::iterator iter;
 
-	
-	cout << "주인공은 " << m["김신"] << endl;
+    //벡터의 원소들을 출력 (iterator)
+    //삽입할떄마다 key는 자동정렬
+    for (auto iter = m.begin(); iter != m.end(); iter++)
+    {
+        cout << iter->first << " 역 " << iter->second << " 분 " << endl;
+    }
+    cout << endl;
 
-	
-	return 0;
+    map<string, string>::iterator actor = m.find("김신");
 
+    //key가 존재하는지 체크
+    if (actor != m.end())
+        cout << "주인공은" << actor->second << endl;
+
+    return 0;
 }
